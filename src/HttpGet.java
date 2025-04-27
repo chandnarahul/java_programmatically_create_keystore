39d50c35e4df8cd86fa54c282bf5d69dc0ac088c
Rob McDougall <rmcdougawww@rogers.com>
2025-04-27 10:21:20 -0400

    Removed unnecessary imports

--- src/HttpGet.java
+++ src/HttpGet.java
@@ -1,7 +1,4 @@
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URI;
